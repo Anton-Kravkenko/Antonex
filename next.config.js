7e64a19a6e10448d40e3fc5fcfb59bffffe60b84
Anton <antonkzavcenco300@gmail.com>
2023-07-27 11:44:08 +0200

    fix next js build error

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
 		domains: ['avatars.githubusercontent.com']
 	},
 	experimental: {
-		outputFileTracingIgnores: ['**canvas**', '**swc/core**']
+		outputFileTracingIgnores: ['**canvas**', '**swc/core**'],
+		outputFileTracingExcludes: {
+			'*': [
+				'node_modules/@swc/core-linux-x64-gnu',
+				'node_modules/@swc/core-linux-x64-musl',
+				'node_modules/@esbuild/linux-x64'
+			]
+		}
 	}
 }
 
